b73ba0f240bb04557da0625c199743a4698844a3
SUMIT <sumitdogra3236@gmail.com>
2021-05-25 10:49:47 +0530

    Reverse String

---
+++ Easy/Reverse_String.java
@@ -0,0 +1,16 @@
+// Reverse the string given in array of characters
+//  https://leetcode.com/problems/reverse-string/
+class Solution {
+    public void reverseString(char[] s) {
+        int low = 0;
+        int high = s.length - 1;
+        while (low < high) {
+            char temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low++;
+            high--;
+        }
+
+    }
+}
